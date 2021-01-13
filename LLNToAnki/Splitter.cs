87c8ce4f8a8d50b3edebd84dad4e3ffeff6e785d
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-01-13 14:21:11 +0100

    Create splitter on \t

---
+++ LLNToAnki/Splitter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Splitter
+{
+    public List<string> Split(string text)
+    {
+        var separator = "\t";
+
+        var r = text.Split(separator).ToList();
+
+        return r;
+    }
+}
--- ZXTests/S001_Basics.cs
+++ ZXTests/S001_Basics.cs
@@ -5,22 +5,33 @@ namespace ZXTests
 {
     public class Tests
     {
+        string singleWord_thouURL;
+
         [SetUp]
         public void Setup()
         {
+            singleWord_thouURL = @"C:\Users\felix\source\repos\LLNToAnki\ZXTests\Data\singleWord_thou.csv";
         }
 
         [Test]
         public void T001_ReadLocalFile()
         {
-            //Arrange
-            string url = @"C:\Users\felix\source\repos\LLNToAnki\ZXTests\Data\singleWord_thou.csv";
-
             //Act
-            string text = new Reader().Read(url);
+            string text = new Reader().Read(singleWord_thouURL);
 
             //Assert
             Assert.AreEqual("\"<style>\n\n    html,\n    body {", text.Substring(0,30));
+        }
+
+        [Test]
+        public void T002_SplitStringWithBackSlashT()
+        {
+            //Arrange
+            string text = new Reader().Read(singleWord_thouURL);
+
+            var r = new Splitter().Split(text);
+
+            Assert.AreEqual(3, r.Count);
         }
     }
 }
