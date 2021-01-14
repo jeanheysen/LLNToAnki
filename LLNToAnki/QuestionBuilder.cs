838eff1887010c22f6626d1de393f9d6a4fed1d1
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-01-14 11:33:01 +0100

    Create QuestionBuilder that removes the translation

--- LLNToAnki/QuestionBuilder.cs
+++ LLNToAnki/QuestionBuilder.cs
@@ -1,9 +1,26 @@
-﻿namespace LLNToAnki
+﻿using HtmlAgilityPack;
+
+namespace LLNToAnki
 {
     public class QuestionBuilder
     {
-        public string Build(HtmlAgilityPack.HtmlNode htmlContent)
+        public string Build(HtmlNode htmlContent)
         {
+            var htmldoc = new HtmlDocument();
+
+            HtmlNode nodeToRemove = null;
+            foreach (var node in htmlContent.Descendants())
+            {
+                foreach (var attribute in node.Attributes)
+                {
+                    if (attribute.Name.Contains("dc-translation"))
+                    {
+                        nodeToRemove = node;
+                    }
+                }
+            }
+            nodeToRemove?.Remove();
+
             return htmlContent.OuterHtml;
         }
     }
