0fd914b94323943e5563cb89546addbd361f0cf2
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-02-16 18:53:03 +0100

    add

---
+++ LLNToAnki/WordItemExtractor.cs
@@ -0,0 +1,38 @@
+﻿namespace LLNToAnki
+{
+    public class WordItemExtractor
+    {
+        private readonly HTMLInterpreter interpreter;
+
+        public WordItemExtractor(HTMLInterpreter interpreter)
+        {
+            this.interpreter = interpreter;
+        }
+
+        public string GetTitle()
+        {
+            var node = interpreter.GetNodeByNameAndAttribute("div", "dc-title\"\"");
+            return node.LastChild.InnerText;
+        }
+
+        public string GetWord()
+        {
+            var node = interpreter.GetNodeByNameAndAttribute("span", "dc-gap\"\"");
+            return node.LastChild.InnerText;
+        }
+
+        public string GetQuestion()
+        {
+            var a1 = interpreter.Html;
+            var a2 = a1.Replace("{{c1::", "");
+            var a3 = a2.Replace("}}", "");
+            return a3;
+        }
+
+        public string GetTranslation()
+        {
+            var node = interpreter.GetNodeByNameAndAttribute("div", "dc-translation");
+            return node.LastChild.InnerText;
+        }
+    }
+}
