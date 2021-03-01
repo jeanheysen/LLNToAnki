fa1458fcb42d51568041744a3e2724aacaf91977
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-03-01 13:58:13 +0100

    Scrap local wordreference page

--- ZXTests/S002_ScrapWordReference.cs
+++ ZXTests/S002_ScrapWordReference.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using LLNToAnki.Infrastructure;
 using NUnit.Framework;
 using System.Net;
 using System.Net.Http;
@@ -8,78 +9,38 @@ namespace ZXTests
 {
     public class S002_ScrapWordReference
     {
-        string wordReferenceURL = @"https://www.wordreference.com/enfr/eyeballs";
-        string localWRUrl = @"C:\Users\felix\source\repos\RogerHanain\PXApi\TestPXEngine\Data\WordReferencePages\bread.html";
-        string urlNL = @"https://www.mijnwoordenboek.nl/vertaal/NL/FR/brood";
+        string localWordReferenceEyball = @"C:\Users\felix\source\repos\LLNToAnki\Dictionaries\WordReference\eyeball - English-French Dictionary WordReference.com.htm";
+
+        public HtmlDocument CreateHtmlDocument(string url)
+        {
+            var web = new HtmlWeb();
+
+            return web.Load(url);
+        }
 
         [Test]
-        public async Task T001_ExtractWordReferenceTranslation()
+        public void T001_LoadEyeBallWordReferenceFully()
+        {
+            //Act
+            var document = CreateHtmlDocument(localWordReferenceEyball);
+
+            //Assert
+            Assert.Greater(document.DocumentNode.InnerLength, 120000);
+        }
+
+        [Test]
+        public void T002_ExtractPrincipalTranslationFromPage()
         {
             //Arrange
+            var document = CreateHtmlDocument(localWordReferenceEyball);
+            var scraper = new HTMLScraper();
 
-            //Act
-            //var node = await GetFromWeb(wordReferenceURL);
-            //var node = await GetFromWeb(urlNL);
-            var node = await GetFromWeb(localWRUrl);
+            //act
+            var node = scraper.GetNodeByNameAndAttribute(document.DocumentNode, "table", "class");
 
-            //Act
-            Assert.IsNotNull(node);
-
-            //var web = new HtmlAgilityPack.
-            //
-            ////Act
-            //web.Load(wordReferenceURL);
-            //var node = document.DocumentNode;
-            //var scrapper = new HTMLScraper();
-            //scrapper.GetNodeByNameAndAttribute(node, "table", "class=\"WRD\"");
-        }
-
-        private async Task<HtmlNode> GetFromWeb(string url)
-        {
-            // instance or static variable
-            var client = new HttpClient();
-
-
-            // get answer in non-blocking way
-            using (var response = await client.GetAsync(url))
-            {
-                using (var content = response.Content)
-                {
-                    // read answer in non-blocking way
-                    var result = await content.ReadAsStringAsync();
-
-                    var document = new HtmlDocument();
-
-                    document.LoadHtml(result);
-
-                    return document.DocumentNode;
-                    //var nodes = document.DocumentNode.SelectNodes("Your nodes");
-                    //Some work with page....
-                }
-            }
-        }
-
-        [Test]
-        public void T002_SaveHtmLocally()
-        {
-            WebClient client = new WebClient();
-            string downloadString = client.DownloadString(wordReferenceURL);
-        }
-
-        private void Page_Load(object sender, System.EventArgs e)
-        {
-            //System.IO.StringWriter oStringWriter = new System.IO.StringWriter();
-            //HtmlTextWriter oHtmlTextWriter = new
-            //System.Web.UI.HtmlTextWriter(oStringWriter);
-            //Page.RenderControl(oHtmlTextWriter);
-            //oHtmlTextWriter.Flush();
-            //System.IO.FileStream fs = new
-            //System.IO.FileStream(@"C:\temp\test.htm", System.IO.FileMode.Create);
-            //string s = oStringWriter.ToString();
-            //byte[] b = System.Text.Encoding.UTF8.GetBytes(s);
-            //fs.Write(b, 0, b.Length);
-            //fs.Close();
-            //Response.End();
+            //Assert
+            StringAssert.Contains("The human eyeball is not perfectly spherical.", node.InnerText);
+            StringAssert.Contains("globe oculaire", node.InnerText);
         }
     }
 }
