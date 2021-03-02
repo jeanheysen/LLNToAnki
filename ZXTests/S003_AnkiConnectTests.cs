77c3a909cfddcde92f54d408a01441afa3989b2d
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-03-02 19:25:03 +0100

    All tests ok

--- ZXTests/S003_AnkiConnectTests.cs
+++ ZXTests/S003_AnkiConnectTests.cs
@@ -28,7 +28,7 @@ namespace ZXTests
 
         public string GetJsonContent()
         {
-            string content = "{\"action\":\"addNote\",\"version\":6,\"params\":{\"note\":{\"deckName\":\"All\",\"modelName\":\"Full_Recto_verso_before_after_Audio\",\"fields\":{\"Question\":\"front content\",\"Answer\":\"back content\",\"After\":\"blabla\"},\"options\":{\"allowDuplicate\":false,\"duplicateScope\":\"deck\",\"duplicateScopeOptions\":{\"deckName\":\"All\",\"checkChildren\":false}},\"picture\":[{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/A_black_cat_named_Tilly.jpg/220px-A_black_cat_named_Tilly.jpg\",\"filename\":\"black_cat.jpg\",\"skipHash\":\"8d6e4646dfae812bf39651b59d7429ce\",\"fields\":[\"Back\"]}]}}}";
+            string content = "{\"action\":\"addNote\",\"version\":6,\"params\":{\"note\":{\"deckName\":\"All\",\"modelName\":\"Full_Recto_verso_before_after_Audio\",\"fields\":{\"Question\":\"front content\",\"Answer\":\"back content\",\"After\":\"blabla\",\"Source\":\"\",\"Audio\":\"\"},\"options\":{\"allowDuplicate\":false,\"duplicateScope\":\"deck\",\"duplicateScopeOptions\":{\"deckName\":\"All\",\"checkChildren\":false}},\"picture\":[{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/A_black_cat_named_Tilly.jpg/220px-A_black_cat_named_Tilly.jpg\",\"filename\":\"black_cat.jpg\",\"skipHash\":\"8d6e4646dfae812bf39651b59d7429ce\",\"fields\":[\"Back\"]}]}}}";
             return content;
         }
 
@@ -37,10 +37,12 @@ namespace ZXTests
         public void T001_BuildContentWithJsonConverterReturnsSameResultWithCase()
         {
             //Arrange
-            var ankiNoteMock = new Mock<AnkiNote>() { DefaultValue = DefaultValue.Mock };
+            var ankiNoteMock = new Mock<IAnkiNote>() { DefaultValue = DefaultValue.Mock };
             ankiNoteMock.SetupGet(a => a.Question).Returns("front content");
             ankiNoteMock.SetupGet(a => a.Answer).Returns("back content");
             ankiNoteMock.SetupGet(a => a.After).Returns("blabla");
+            ankiNoteMock.SetupGet(a => a.Audio).Returns("");
+            ankiNoteMock.SetupGet(a => a.Source).Returns("");
             var note = connectNoteBuilder.Build(ankiNoteMock.Object);
 
             //Act
