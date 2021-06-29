4cda4003d835c8b20d81f61d0fa58b0923c2c760
Felix <55741682+RogerHanain@users.noreply.github.com>
2021-06-29 11:12:42 +0200

    declare DataContext

--- FE/LLNToAnki.WPF/MainWindow.xaml.cs
+++ FE/LLNToAnki.WPF/MainWindow.xaml.cs
@@ -1,33 +1,18 @@
-﻿using LLNToAnki.WPF.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LLNToAnki.WPF.Client;
+using LLNToAnki.WPF.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LLNToAnki.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public FlowPageVM ViewModel { get; }
-        
+        public FlowPageVM ViewModel { get => (FlowPageVM)DataContext; }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            this.ViewModel = DataContext as FlowPageVM;
+            DataContext = new FlowPageVM(new FacadeClient(null));
         }
 
     }
